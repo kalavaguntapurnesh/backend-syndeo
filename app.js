a67fac740a4036e9d6a118a2568e19ea7f44b30f
@Purnesh1226 <purnesh.kalavagunta@clouddatanetworks.com>
2024-12-31 15:57:59 +0530

    Protected route

--- app.js
+++ app.js
@@ -13,11 +13,19 @@ console.log("Conncected to mongodb. Now going further...");
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+// app.use(cors());
+
+app.use(
+  cors({
+    origin: ["https://syndeo-frontendtwo.vercel.app/"], // Add your frontend's URL here
+    credentials: true, // Allow cookies and Authorization headers
+    methods: ["GET", "POST", "PUT", "DELETE"], // Specify allowed methods
+    allowedHeaders: ["Content-Type", "Authorization"], // Allow Authorization header
+  })
+);
 
 app.use(cookieParser());
 app.use(morgan("dev"));
-
 app.use("/api/v1", userRoutes);
 
 app.use((error, req, res, next) => {
